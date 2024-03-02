5451ffbfaa022d026ab2ee3c3115f71cdc1ca26a
heowen <heowen@udel.edu>
2024-03-01 23:56:28 -0500

    finished task

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,45 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holiday = "🎅" | "🦃" | "🎃" | "🥚" | "🍀";
+    const alphabeticalHoliday: Record<Holiday, string> = {
+        "🥚": "Christmas",
+        "🎅": "Thanksgiving",
+        "🦃": "Halloween",
+        "🍀": "Easter",
+        "🎃": "St. Patrick's Day"
+    };
+    const dateHoliday: Record<Holiday, string> = {
+        "🍀": "Christmas",
+        "🎅": "Thanksgiving",
+        "🦃": "Halloween",
+        "🎃": "Easter",
+        "🥚": "St. Patrick's Day"
+    };
+    const mapHoliday: Record<string, Holiday> = {
+        Christmas: "🎅",
+        Thanksgiving: "🦃",
+        Halloween: "🎃",
+        Easter: "🥚",
+        "St. Patrick's Day": "🍀"
+    };
+    const [holiday, setHoliday] = useState<Holiday>("🎅");
+    return (
+        <div>
+            Cycle Holiday
+            <span>{"Holiday: " + holiday}</span>
+            <Button
+                onClick={() =>
+                    setHoliday(mapHoliday[alphabeticalHoliday[holiday]])
+                }
+            >
+                Advance by Alphabet
+            </Button>
+            <Button
+                onClick={() => setHoliday(mapHoliday[dateHoliday[holiday]])}
+            >
+                Advance by Year
+            </Button>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
@@ -12,5 +13,28 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [dieOne, setLeftDie] = useState<number>(1);
+    const [dieTwo, setRightDie] = useState<number>(2);
+
+    function rollLeftDie(): void {
+        setLeftDie(d6);
+    }
+    function rollRightDie(): void {
+        setRightDie(d6());
+    }
+    return (
+        <div>
+            <Button onClick={rollLeftDie}>Roll Left</Button>
+            <Button onClick={rollRightDie}>Roll Right</Button>
+            <span>
+                {dieOne === 1 && dieTwo === 1
+                    ? "Lose"
+                    : dieOne === dieTwo
+                    ? "Win"
+                    : ""}
+            </span>
+            <span data-testid="right-die">{dieTwo}</span>
+            <span data-testid="left-die">{dieOne}</span>
+        </div>
+    );
 }
